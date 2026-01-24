1daeeff9bcdfd3e4d43b4d3f4b3d1845d28acda3
haritswijaya95 <haritswijaya95@github.com>
2026-01-24 02:57:20 -0800

    Setup Laravel for Vercel deployment

--- api/index.php
+++ api/index.php
@@ -1,20 +1,11 @@
 <?php
 
-use Illuminate\Foundation\Application;
-use Illuminate\Http\Request;
+// 1. Arahkan ke vendor autoload
+require __DIR__ . '/../vendor/autoload.php';
 
-define('LARAVEL_START', microtime(true));
+// 2. Jalankan bootstrap Laravel
+// File ini akan menginisialisasi kernel Laravel
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
-
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$app->handleRequest(Request::capture());
+// 3. Tangani request yang masuk
+$app->handleRequest(Illuminate\Http\Request::capture());
